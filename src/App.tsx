8247c9980bf8a8c2fd520018023779d2e92b2626
dahora328 <rodrigoliveira328@hotmail.com>
2025-11-18 11:18:29 -0300

    Feat add login in routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import {
+  Route,
+  BrowserRouter as Router,
+  Routes,
+  useLocation,
+} from 'react-router-dom';
 import { TopBar } from './components/Menu/TopBar';
 import './styles/App.css';
 import { Athletes } from './pages/Athletes';
-import { Home } from './pages/Home';
 import { User } from './pages/User';
 import { Reports } from './pages/Reports';
+import { LandingPage } from './pages/LandingPage';
+import { Home } from './pages/Home';
+import { Login } from './pages/User/Login';
+
+function AppContent() {
+  const location = useLocation();
+  const hideTopBar =
+    location.pathname === '/' || location.pathname === '/login';
+
+  return (
+    <>
+      {!hideTopBar && <TopBar />}
+      <Routes>
+        {/* Páginas iniciais */}
+        <Route path='/' element={<LandingPage />} />
+        <Route path='/user' element={<User />} />
+        {/* Páginas principais */}
+        <Route path='/reports' element={<Reports />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/athletes' element={<Athletes />} />
+      </Routes>
+    </>
+  );
+}
 
 function App() {
   return (
-    <>
-      <Router>
-        <TopBar />
-        <Routes>
-          <Route path='/reports' element={<Reports />} />
-          <Route path='/user' element={<User />} />
-          <Route path='/' element={<Home />} />
-          <Route path='/athletes' element={<Athletes />} />
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <AppContent />
+    </Router>
   );
 }
 

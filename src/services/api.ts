4d58c40e150a189b2250712574023f146eb9f8ed
rodrigo <rodrigoliveira328@hotmail.com>
2026-08-23 15:54:39 -0300

    test

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@
 import axios, { AxiosError, type AxiosRequestConfig } from 'axios';
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080/api/',
+  baseURL: import.meta.env.local.VITE_API_URL || 'http://localhost:8080/api/',
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json',
